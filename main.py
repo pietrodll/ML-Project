daf112d732744feeffcd705ca418c61197f40b80
Pietro Dellino <pietro.dellino@student.ecp.fr>
2019-01-05 18:31:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn import linear_model, neural_network
 from math import sqrt
 from validation import crossValidation
 import preprocessing as pre
@@ -24,6 +25,7 @@ def KNNregression(trainSet, trainLabels, testSet):
     y = neigh.predict(testSet)
     return np.exp(y)
 
+<<<<<<< HEAD
 def SVMregression(X, y, Xtest):
     clf = svm.SVR(kernel='poly')
     clf.fit(X, y)
@@ -32,6 +34,22 @@ def SVMregression(X, y, Xtest):
 
 # def linearRegression(trainSet, trainLabels, testSet):
     
+=======
+def linearRegression(trainSet, trainLabels, testSet):
+    regr = linear_model.LinearRegression()
+    regr.fit(trainSet,trainLabels)
+    return regr.predict(testSet)
+
+def stochGrad(trainSet, trainLabels, testSet):
+    regr = linear_model.SGDRegressor()
+    regr.fit(trainSet,trainLabels)
+    return regr.predict(testSet)
+
+def neuralNetwork(trainSet, trainLabels, testSet):
+    neu=neural_network.MLPRegressor()
+    neu.fit(trainSet,trainLabels)
+    return neu.predict(testSet)
+>>>>>>> a4f4c204a8a737230dd8fda0b3e4f6251347570e
 
 def plotFeatureSelection(X, y, regressionFunction):
     error = [0]*X.shape[1]
@@ -40,4 +58,43 @@ def plotFeatureSelection(X, y, regressionFunction):
         # Xnew = pre.PCA(X, k)
         error[k-1] = crossValidation(Xnew, y, regressionFunction)
     error[-1] = crossValidation(X, y, KNNregression)
+<<<<<<< HEAD
     plt.plot(np.arange(1, X.shape[1]+1), error)
+=======
+    plt.plot(list(range(1, X.shape[1]+1)), error,color='blue')
+    
+    error2 = [0]*X.shape[1]
+    for k in range(1, X.shape[1]):
+        Xnew = pre.featureSelect(X, y, k)
+        # Xnew = pre.PCA(X, k)
+        error2[k-1] = crossValidation(Xnew, y, linearRegression)
+    error2[-1] = crossValidation(X, y, linearRegression)
+    plt.plot(list(range(1, X.shape[1]+1)), error2, color='green')
+    
+    error3 = [0]*X.shape[1]
+    for k in range(1, X.shape[1]):
+        Xnew = pre.featureSelect(X, y, k)
+        #Xnew = pre.PCA(X, k)
+        error3[k-1] = crossValidation(Xnew, y, stochGrad)
+    error3[-1] = crossValidation(X, y, stochGrad)
+    plt.plot(list(range(1, X.shape[1]+1)), error3, color='red')
+    
+    error4 = [0]*X.shape[1]
+    for k in range(1, X.shape[1]):
+        Xnew = pre.featureSelect(X, y, k)
+        #Xnew = pre.PCA(X, k)
+        error4[k-1] = crossValidation(Xnew, y, neuralNetwork)
+    error4[-1] = crossValidation(X, y, neuralNetwork)
+    plt.plot(list(range(1, X.shape[1]+1)), error4, color='orange')
+    
+def displayResult(X,y,k):
+    Xnew = pre.featureSelect(X,y,k)
+    print("Neighbors : ")
+    crossValidation(Xnew,y,KNNregression,display=True)
+    print("Linear Regression : ")
+    crossValidation(Xnew,y,linearRegression,display=True)
+    print("Stochastic : ")
+    crossValidation(Xnew,y,stochGrad,display=True)
+    print("Neural network : ")
+    crossValidation(Xnew,y,neuralNetwork,display=True)
+>>>>>>> a4f4c204a8a737230dd8fda0b3e4f6251347570e
